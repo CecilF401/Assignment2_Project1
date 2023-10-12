547deb90ec00482b3f7949cceecd37d12911b0f5
CecilF401 <130788814+CecilF401@users.noreply.github.com>
2023-10-12 10:31:22 -0400

    Started making unit test

---
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" int getPerimeter(int, int);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BCSRecTests
+{
+	TEST_CLASS(BCSRecTests)
+	{
+	public:
+		
+		TEST_METHOD(GetPerimeterFunctionality)
+		{
+			int result = 0;
+			result = 
+		}
+		TEST_METHOD(GetAreaFunctionality) {
+
+		}
+	};
+}
